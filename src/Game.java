da0c0373cbe426ffa3a2c62e46766de3031e5441
Chinmay Bhalodia <94515096+chinmaybhalodia@users.noreply.github.com>
2023-12-01 21:52:24 +0530

    added count of steps and print moves of game

--- src/Game.java
+++ src/Game.java
@@ -1,16 +1,22 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Game {
     int position;
+    int dice_rolls;
+    ArrayList<Integer> positions;
 
     public Game() {
         this.position = 0;
+        this.dice_rolls = 0;
+        this.positions = new ArrayList<Integer>(); // UC6
+        this.positions.add(0);
     }
 
     // UC3: function to get next posiion of the player
     public void rollDice() {
         Random random = new Random();
-        int dice = random.nextInt(7);
+        int dice = random.nextInt(6) + 1;
         int choice = random.nextInt(3);
 
         switch (choice) {
@@ -35,6 +41,12 @@ public class Game {
             default:
                 break;
         }
+
+        // UC6: count number of dice rolls of game.
+        this.dice_rolls++;
+
+        // UC6: keep track of every move
+        this.positions.add(this.position);
     }
 
     // UC4: function to play the game for single player until win
@@ -42,6 +54,12 @@ public class Game {
         while (this.position < 100) {
             this.rollDice();
         }
-        System.out.println("Game Over. Player Won!");
+        System.out.println("Game Over. Player Won in " + this.dice_rolls + " dice rolls.");
+        this.printMoves();
+    }
+
+    // UC6: print all current moves
+    public void printMoves() {
+        System.out.println("Moves of this game: " + this.positions.toString());
     }
 }
